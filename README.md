daa0b1af5e35bad2d577b36fbc257a601d816231
andrus <aadamchik@gmail.com>
2013-05-25 16:51:07 +0300

    launcher

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 jetty-launcher
 ==============
 
-A launcher that runs a Java web app from a simple "main" method. Its primary purpose is to run Java web apps in Eclipse.
+"jetty-launcher" is for Java web app developers who are using Maven and Eclipse. Its advantage over ["jetty-maven-plugin"](http://wiki.eclipse.org/Jetty/Feature/Jetty_Maven_Plugin) is that "jetty-launcher" does not require recompiling the dependencies of the web app project from the command line. It also works in debugger. Both are pretty important things.
 
-What's Included
----------------
+The launcher is NOT an Eclipse plugin, even though it is most useful under Eclipse. It is a simple Java class with a "main" method that runs a single Java web app.
 
-'jetty-launcher' 1.0 is based on Jetty 8 and supports Java 1.6 and Servlet Spec 3.0. 
+What's Included and What's Not Encluded
+---------------------------------------
 
-As it happens I care very little about JSP, JSF, EL and other common web container things. So those specs are exlcuded. It should be very easy to add them back by declaring corresponding artifacts in the pom.
+v1.0 is based on Jetty 8 and supports Java 1.6 and Servlet Spec 3.0. The launcher is intentionally crippled based on my personal Java webapp stack preferences. 
+
+What's included:
+
+* Servlets / filters and other web.xml goodies.
+* JNDI
+
+What's NOT included:
+
+* As it happens I care very little about JSP, JSF, EL so those are excluded.
+* No support for EJBs or other heavier JEE stuff.
+* Servlet 3.0 annotation processing.
+* Clean shutdown and hot redeploy (another thing I don't care about in development)
+
+Patches to add these are welcomed!
 
 Usage
 -----
@@ -17,14 +31,14 @@ Usage
 
 * Add dependency on jetty-launcher to your web project, setting the scope as "provided" (i.e. you don't want jetty-laucher.jar end up in your .war during deployment).
 
-* In Eclipse, right-click on your web project and select "Run As > Java Application". Select "org.objectstyle.jetty.Launcher" as your main class and click run. Check your app at (http://localhost:8080/)[http://localhost:8080/].
+* In Eclipse, right-click on your web project and select "Run As > Java Application". Select "org.objectstyle.jetty.Launcher" as your main class and click run. Check your app at (http://localhost:8080/mymodulename)[http://localhost:8080/mymodulename].
 
 Customization
 -------------
 
 jetty-launcher supports two properties that can be passed on the command line with "-D":
 
-* os.jetty.context - the name of the webapp context. Default is "/"
+* os.jetty.context - the name of the webapp context. Default is the name of the project module.
 * os.jetty.port - the port to listen on. Default is "8080"
 
 E.g.:
--- src/main/java/org/objectstyle/jetty/Launcher.java
+++ src/main/java/org/objectstyle/jetty/Launcher.java
@@ -1,11 +1,102 @@
+/*****************************************************************
+ *  Licensed under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
 package org.objectstyle.jetty;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Launcher {
 
 	private static final String CONTEXT_PROPERTY = "os.jetty.context";
 	private static final String PORT_PROPERTY = "os.jetty.port";
 
-	public static void main(String[] args) {
+	private static final String WAR_PATH = "src" + File.separator + "main" + File.separator + "webapp";
 
+	public static void main(String[] args) throws Exception {
+		new Launcher().launch();
+	}
+
+	void launch() throws Exception {
+		int port = getPort();
+
+		Server server = new Server();
+
+		SelectChannelConnector connector = new SelectChannelConnector();
+		connector.setPort(port);
+		server.addConnector(connector);
+
+		WebAppContext context = createContext();
+
+		// this is needed to get stuff from Eclipse project CLASSPATH
+		context.setParentLoaderPriority(true);
+
+		ContextHandlerCollection contexts = new ContextHandlerCollection();
+
+		contexts.addHandler(context);
+
+		server.setHandler(contexts);
+		server.setStopAtShutdown(true);
+		server.setSendServerVersion(true);
+
+		server.start();
+	}
+
+	private WebAppContext createContext() throws MalformedURLException {
+
+		File projectBase = new File(System.getProperty("user.dir"));
+
+		File warFolder = new File(projectBase, WAR_PATH);
+		String warUrl = warFolder.toURI().toURL().toExternalForm();
+
+		WebAppContext context = new WebAppContext();
+
+		context.setWar(warUrl);
+		String contextPath = System.getProperty(CONTEXT_PROPERTY);
+		if (contextPath == null) {
+			contextPath = "/" + projectBase.getName();
+		}
+
+		context.setContextPath(contextPath);
+
+		// "plus" configs
+		context.setConfigurationClasses(new String[] { "org.eclipse.jetty.webapp.WebInfConfiguration",
+				"org.eclipse.jetty.webapp.WebXmlConfiguration", "org.eclipse.jetty.webapp.MetaInfConfiguration",
+				"org.eclipse.jetty.webapp.FragmentConfiguration", "org.eclipse.jetty.plus.webapp.EnvConfiguration",
+				"org.eclipse.jetty.plus.webapp.PlusConfiguration",
+				"org.eclipse.jetty.annotations.AnnotationConfiguration",
+				"org.eclipse.jetty.webapp.JettyWebXmlConfiguration" });
+
+		return context;
+	}
+
+	private int getPort() {
+		String stringPort = System.getProperty(PORT_PROPERTY);
+
+		if (stringPort == null) {
+			return 8080;
+		}
+
+		try {
+			return Integer.parseInt(stringPort);
+		} catch (NumberFormatException nfex) {
+			throw new RuntimeException("Non-numeric port specified for '" + PORT_PROPERTY + "': " + stringPort);
+		}
 	}
 }
